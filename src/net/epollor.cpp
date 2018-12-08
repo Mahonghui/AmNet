89f854706a21eb28cfb5c5ff877b87a30ea1cafd
Mahonghui <amisher@163.com>
2018-12-08 16:39:10 +0800

    wrap epoll sys_call

---
+++ src/net/epollor.cpp
@@ -0,0 +1,87 @@
+//
+// Created by amisher on 18-12-8.
+//
+#include "eventbase.h"
+#include "epollor.h"
+#include "util.h"
+#include "logger.h"
+
+// 监听事件上限
+const int EVENTS_MAX = 4096;
+// wait超时时间
+const int EPOLL_WAIT_TIME = 10000;
+
+Epollor::Epollor(): epoll_fd_(epoll_create1(EPOLL_CLOEXEC)), active_event_(EVENTS_MAX) {
+    if(epoll_fd_ < 0)
+    {
+        LOG_FATAL<<"fail to create epoll_fd_ ";
+    }
+}
+
+Epollor::~Epollor() {
+    util::Close(epoll_fd_);
+}
+
+void Epollor::Add(std::shared_ptr <EventBase> eb) {
+
+    int fd = eb->getFd();
+    struct epoll_event event;
+    event.events = eb->getEvents();
+    event.data.fd = fd;
+
+    fd2eb[fd] = eb;
+    // 注册到epoll事件表
+    if(spoll_ctl(epoll_fd_,EPOLL_CTL_ADD, fd, &event) < 0)
+    {
+        // 注册失败
+        LOG_ERROR<<"fail to invoke epoll_ctl[ADD]";
+        fd2eb.erase(fd);
+    }
+}
+
+void Epollor::Mod(std::shared_ptr <EventBase> eb) {
+    int fd = eb->getFd();
+    struct epoll_event event;
+    event.events = eb->getEvents();
+    event.data.fd = fd;
+
+    if(epoll_ctl(epoll_fd_, EPOLL_CTL_MOD, fd, &event)<0)
+    {
+        LOG_ERROR<<"fail to invoke epoll_ctl[MOD]";
+        fd2eb.erase(fd);
+    }
+
+}
+
+void Epollor::Del(std::shared_ptr <EventBase> eb) {
+    int fd = eb->getFd();
+    struct epoll_event event;
+    event.events = eb->getEvents();
+    event.data.fd = fd;
+
+    if(epoll_ctl(epoll_fd_, EPOLL_CTL_DEL, fd, &event) < 0)
+    {
+        LOG_ERROR<< "fail to invoke epoll_ctl[DEL]";
+    }
+    fd2eb.earse(fd);
+}
+
+std::vector<std::shared_prt<EventBase>> Epollor::Poll() {
+    // active_event中接受活跃的epoll_event
+    int active_event_count = spoll_wait(epoll_fd_, &*active_event_.begin(), active_event_.size(), EPOLL_WAIT_TIME);
+
+    if(active_event_count<0)
+    {
+        LOG_ERROR<<"Error in epoll_wait()";
+    }
+
+    std::vector<std::shared_ptr<EventBase>> active_events_list;
+    for(int i=0; i<active_event_count; ++i)
+    {
+        //按就绪的socket，找对应的事件智能指针
+        std::shared_ptr<EventBase> eb = fd2eb[active_event_[i].data.fd];
+        active_events_list.push_back(eb);
+    }
+    return active_events_list;
+}
+
---
+++ src/net/epollor.h
@@ -0,0 +1,38 @@
+//
+// Created by amisher on 18-12-8.
+//
+
+#ifndef AMNET_EPOLLOR_H
+#define AMNET_EPOLLOR_H
+
+
+#include <sys/epoll.h>
+#include <vector>
+#include <map>
+#include <memory>
+
+class EventBase;
+
+class Epollor{
+public:
+    Epollor(){};
+    ~Epollor(){};
+
+    // 注册，修改，删除事件
+    void Add(std::shared_ptr<EventBase> eb);
+    void Mod(std::shared_ptr<EventBase> eb);
+    void Del(std::shared_ptr<EventBase> eb);
+
+
+    // 等待事件
+    std::vector<std::shared_ptr<EventBase>> Poll();
+
+private:
+    // epoll描述符，标识内核中的epoll事件表
+    int epoll_fd_;
+    // 负责接受epoll_wait返回的就绪事件
+    std::vector<epoll_event> active_event_;
+    // socket到eventbase的映射，根据就绪的socket找到具体的事件处理器
+    std::map<int, std::shared_ptr<EventBase>> fd2eb;
+};
+#endif //AMNET_EPOLLOR_H
