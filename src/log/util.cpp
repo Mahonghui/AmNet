0aaa1866f0d2731f65dd770862e359709b36fe0f
Mahonghui <amisher@163.com>
2018-12-09 16:20:07 +0800

    简单文件操作的封装

---
+++ src/log/util.cpp
@@ -0,0 +1,45 @@
+//
+// Created by amisher on 18-12-9.
+//
+
+#include <string.h>
+#include <stdio.h>
+#include "util.h"
+
+File::File(const std::string filename){
+    if((fp_ = fopen(filename.c_str(), "ae")) == nullptr)
+        printf("fail to open logfile: %d[%s]", errno, strerror(errno));
+    std::setvbuf(fp_, buf_);
+}
+
+File::~File() {
+    fclose(fp_);
+}
+
+void File::Append(const char *str, size_t len) {
+    size_t  n = Write(str, len);
+    size_t remain = len - n;
+    while (remain>0)
+    {
+        size_t temp = Write(str+n, remain);
+        if(temp == 0)
+        {
+            int e = ferror(fp_);
+            if(e)
+                fprintf(stderr, "Append error: %d[%s]", errno, strerror(errno));
+            break;
+        }
+
+        n += temp;
+        remain = len - n;
+    }
+}
+
+void File::Flush() {
+    fflush(fp_);
+}
+
+size_t File::Write(const char *str, size_t len) {
+    // 只使用一个log线记录日志，因此不要求线程安全性
+    return fwrite_unlocked(str,1,len, fp);
+}
---
+++ src/log/util.h
@@ -0,0 +1,26 @@
+//
+// Created by amisher on 18-12-9.
+//
+
+#ifndef AMNET_UTIL_H
+#define AMNET_UTIL_H
+
+#include <string>
+
+class File{
+public:
+    explicit File(const std::string filename);
+    ~File();
+
+    void Append(const char* str, size_t len);
+
+    void Flush();
+
+private:
+    // 写函数，非线程安全
+    size_t Write(const char* str, size_t len);
+    FILE* fp_;
+    // 文件缓冲区
+    char buf_[1024*128];
+};
+#endif //AMNET_UTIL_H
