08ad17f5faf291ad438a9552045f27275400b1af
Mahonghui <amisher@163.com>
2018-12-18 16:53:19 +0800

    match regular pattern

---
+++ src/http/matcher.cpp
@@ -0,0 +1,97 @@
+//
+// Created by amisher on 18-12-17.
+//
+
+#include <regex>
+#include <iostream>
+#include <utility>
+#include "matcher.h"
+#include "httprequest.h"
+
+Matcher::Matcher(std::string pattern, Matcher::RegexType type, std::string& key):
+valid_(true),
+regex_type_(type),
+key_(key)
+{
+    InitPattern(pattern);
+}
+
+void Matcher::InitPattern(std::string& pattern) {
+    // 判断{} 是否配对
+    int level = 0;
+    unsigned int index =0;
+    std::vector<unsigned int> indexes;
+
+    for(unsigned int i=0; i<pattern.size(); ++i){
+        if(pattern[i] == '{'){
+            if(++level == 1)
+                index = i;
+        }
+        else if(pattern[i] == '}'){
+            if(--level == 0){
+                indexes.push_back(index);
+                indexes.push_back(i+1);
+            }
+            else if(level < 0){
+                valid_ = false;
+                return;
+            }
+        }
+    }
+
+    if(level != 0){
+        valid_ = false;
+        return;
+    }
+    // xx{name: [0-9]+}yy --> xx([0-9]+)yy
+    unsigned int end = 0;
+    for(int i=0; i<indexes.size(); i += 2){
+        std::string r = pattern.substr(end, indexes[i]-end);
+        end = (unsigned)indexes[i+1];
+
+        pattern_.append(r);
+
+        unsigned int j;
+        for(j =indexes[i]; j<end; ++j){
+            if(pattern[j] == ':'){
+                regex_names_.push_back(pattern.substr(indexes[i]+1, j-(indexes[i]+1)));
+                pattern_.append( "(" + pattern.substr(j+1, (end-1)-(j+1))+")");
+                break;
+            }
+        }
+
+        if(j == end){
+            regex_names_.push_back(pattern.substr(indexes[i]+1, (end-1)-(indexes[i]+1)));
+            pattern_.append("(.*)");
+        }
+    }
+
+    if(end < pattern.size()){
+        pattern_.append(pattern.substr(end, pattern.size()-end));
+    }
+}
+
+bool Matcher::isMatched(const HttpRequest &request, std::unordered_map<std::string, std::string> *dict) {
+    std::regex reg(pattern_);
+    std::smatch m;
+
+    if(regex_type_ == REGEX_TYPE_URL || regex_type_ == REGEX_TYPE_PREFIX){
+        if(regex_match(request.GetPath(), m, reg)){
+            // m 中包含匹配的整个字符串和匹配子串
+            for(unsigned long int i=regex_names_.size()-1, j = m.size()-1; i>=0&&j>=0; --i, --j){
+                dict->insert(std::make_pair(regex_names_[i], m[j]));
+            }
+            return true;
+        }
+    }else if(regex_type_ == REGEX_TYPE_METHOD){
+        if(regex_match(request.GetMethodStr(), reg))
+            return true;
+    }else if(regex_type_ == REGEX_TYPE_HEADER){
+        if(regex_match(request.GetHeader(key_), reg))
+            return true;
+    }else if(regex_type_ == REGEX_TYPE_QUERY){
+        if(regex_match(request.GetQuery(key_), reg))
+            return true;
+    }
+    return false;
+}
---
+++ src/http/matcher.h
@@ -0,0 +1,45 @@
+//
+// Created by amisher on 18-12-17.
+//
+
+#ifndef AMNET_MATCHER_H
+#define AMNET_MATCHER_H
+
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+class HttpRequest;
+
+class Matcher{
+public:
+    enum RegexType{
+        REGEX_TYPE_METHOD,
+        REGEX_TYPE_URL,
+        REGEX_TYPE_PREFIX,
+        REGEX_TYPE_HEADER,
+        REGEX_TYPE_QUERY,
+    };
+    Matcher(std::string pattern, RegexType type, std::string& key);
+
+    bool isValid()const { return valid_;}
+
+    bool isMatched(const HttpRequest& request, std::unordered_map<std::string, std::string>* dict);
+
+
+private:
+    void InitPattern(std::string& pattern);
+
+    bool valid_;
+    //进行匹配的类型
+    RegexType regex_type_;
+    // 对头部或query匹配时的key
+    std::string key_;
+    // 正则模式
+    std::string pattern_;
+    // 匹配结果
+    std::vector<std::string> regex_names_;
+
+};
+
+#endif //AMNET_MATCHER_H
