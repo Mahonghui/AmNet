f55033a4a796bbb6dcae7b21be80984cd2fbe1e7
Mahonghui <amisher@163.com>
2018-12-22 15:00:11 +0800

    auto commit

--- src/net/loopthread.cpp
+++ src/net/loopthread.cpp
@@ -19,18 +19,20 @@ LooperThread::~LooperThread() {
 Looper* LooperThread::GetLooper() {
 
     // 条件变量+互斥锁，确保安全获得looper
+    // 单独一个语句块形成临界区
     {
         std::unique_lock<std::mutex> lock(mutex_);
         while (loop_ == nullptr)
         {
             condition_.wait(lock);
         }
-    }
+    }// 区块结束自动析构lock，释放互斥锁
     return  loop_;
 }
 
 void LooperThread::ThreadFunc() {
     Looper loop;
+    // 获得锁的临界区
     {
         std::unique_lock<std::mutex> lock(mutex_);
         loop_ = &loop;
